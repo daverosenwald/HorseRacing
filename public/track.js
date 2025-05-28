b3139f0ecce307e7c3a97c56ff9d3b1395ea9ad8
daverosenwald <54130488+daverosenwald@users.noreply.github.com>
2025-05-28 18:23:51 -0400

    Create track.js

---
+++ public/track.js
@@ -0,0 +1,9 @@
+// track.js - Placeholder for race track logic
+export class Track {
+  constructor(canvas) {
+    this.canvas = canvas;
+  }
+  update(deltaTime) {}
+  render(ctx, playerPosition) {}
+  handleResize(canvas) {}
+}
